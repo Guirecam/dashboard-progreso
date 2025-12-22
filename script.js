4bf06f92bc4b3e3bc8497ab214d67f1363db1729
Guirecam <guillerc18@gmail.com>
2025-12-22 17:15:31 +0100

    Añadir proyectos al archivo JSON y conectar script.js con project.json mostrando en consola

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('funcionando')
+
+fetch("projects.json")
+    .then((response) => response.json())
+    .then((projects) => {
+        console.log("Proyectos cargados del JSON", projects);
+    })
+    .catch((error) => {
+        console.error("Error cargando projects.json", error);
+    });
